c0fa433b7214e3cf7cd067fb0a5cedec375148be
YewKokKin <kinkinboy0414@gmail.com>
2019-12-06 15:39:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.bmi_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btncalculate.setOnClickListener() {
+            try {
+
+
+                val weight: Double = weight.text.toString().toDouble()
+                val height: Double = height.text.toString().toDouble()
+                val bmi = CalculateBMI(weight, height)
+//            if(bmi < 18.5){
+//                imgResult.setImageResource(R.drawable.under)
+//            }
+//            else if(bmi < 24.9){
+//                imgResult.setImageResource(R.drawable.normal)
+//            }
+//            else{
+//                imgResult.setImageResource(R.drawable.over)
+//            }
+                val status: String
+                when {
+                    bmi < 18.5 -> {
+                        imgResult.setImageResource(R.drawable.under)
+                        status = "under"
+                    }
+                    bmi < 24.9 -> {
+                        imgResult.setImageResource(R.drawable.normal)
+                        status = "normal"
+                    }
+                    else -> {
+                        imgResult.setImageResource(R.drawable.over)
+                        status = "over"
+                    }
+                }
+                txtInfo.text = "BMI %.2f (%s)".format(bmi, status)
+            }catch (ex:Exception){
+                val toast: Toast = Toast.makeText(this, "Invalid Input", Toast.LENGTH_LONG)
+                toast.setGravity(Gravity.CENTER, 0, 0)
+                toast.show()
+            }
+        }
+
+
+        btnReset.setOnClickListener(){
+            imgResult.setImageResource(R.drawable.empty)
+            txtInfo.text= ""
+            height.text.clear()
+            weight.text.clear()
+            height.requestFocus()
+        }
+
+
+    }
+
+
+
+    fun CalculateBMI(weight:Double , height:Double):Double{
+
+        return weight/ Math.pow(height, 2.0)
+
+    }
+}
